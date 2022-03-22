8c9471ae2e6418cdf37dc4c72bd6feb604cdb45f
Benedicte Coulon <benedicte.coulon@wanadoo.fr>
2022-03-22 11:08:07 +0100

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  root 'static_pages#index'
+  get 'static_pages/index'
+  get 'static_pages/show'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+end
