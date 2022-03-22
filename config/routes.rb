18c679b9b9cfd2bfdf10dab7b09b1595d176cb24
lexbeaumier <lexbeaumier@gmail.com>
2022-03-22 14:07:04 +0100

    creation page contact + route contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#index'
-  get 'static_pages/index'
-  get 'static_pages/show'
+  get 'static_pages/contact'
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
