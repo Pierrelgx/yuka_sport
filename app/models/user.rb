9c9a2cc66bcf7bf0ada8f39577d0f6a45af9f4bb
Benedicte Coulon <benedicte.coulon@wanadoo.fr>
2022-03-23 15:23:51 +0100

    bug mailer réparé

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
+
+  after_create :welcome_send
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -12,9 +19,5 @@ class User < ApplicationRecord
   presence: true,
   uniqueness: true
 
-  after_create :welcome_send
 
-  def welcome_send
-    UserMailer.welcome_email(self).deliver_now
-  end
 end
