ded43a355b5f48296417f4913098b76c94c581cc
Benedicte Coulon <benedicte.coulon@wanadoo.fr>
2022-03-24 15:07:28 +0100

    seed update

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,32 +1,56 @@
 Brand.create!(
     name: "Nike",
     description: "Nike est un acteur majeur du marché du sportswear. La marque commercialise des vêtements, chaussures, équipements et accessoires de sport. Elle est présente dans plus de 80 pays et compte quelques grands noms dans le monde du football : l'attaquant brésilien Neymar Jr, le milieu de terrain français Paul Pogba, le défenseur allemand Mats Hummels, les attaquants argentins Lionel Messi et Luis Suarez ou encore l’ailier algérien Riyad Mahrez. Le club de football Manchester United est sous contrat avec Nike depuis 2006. En 2012, la société reçoit des critiques pour la suppression d'emplois dans ses usines en Chine.",
-    price: rand(1..99),
-    rank: rand(1..99),
-    quality: rand(1..99),
-    animal: rand(1..99),
-    labour: rand(1..99),
-    environment: rand(1..99)
+    price: rand(0..10),
+    rank: rand(0..10),
+    quality: rand(0..10),
+    animal: rand(0..10),
+    labour: rand(0..10),
+    environment: rand(0..10)
 );
 
 Brand.create!(
   name: "Adidas",
-  description: "Nike est un acteur majeur du marché du sportswear. La marque commercialise des vêtements, chaussures, équipements et accessoires de sport. Elle est présente dans plus de 80 pays et compte quelques grands noms dans le monde du football : l'attaquant brésilien Neymar Jr, le milieu de terrain français Paul Pogba, le défenseur allemand Mats Hummels, les attaquants argentins Lionel Messi et Luis Suarez ou encore l’ailier algérien Riyad Mahrez. Le club de football Manchester United est sous contrat avec Nike depuis 2006. En 2012, la société reçoit des critiques pour la suppression d'emplois dans ses usines en Chine.",
-  price: rand(1..99),
-  rank: rand(1..99),
-  quality: rand(1..99),
-  animal: rand(1..99),
-  labour: rand(1..99),
-  environment: rand(1..99)
+  description: "Adidas est une marque de sport qui s'est fait connaître grâce à la création de nombreux produits de renom, tels que l'adidas Superstar. Depuis sa création en 1948, cette marque a toujours su se démarquer de ses concurrents en proposant des articles de qualité à prix très bas.",
+  price: rand(0..10),
+  rank: rand(0..10),
+  quality: rand(0..10),
+  animal: rand(0..10),
+  labour: rand(0..10),
+  environment: rand(0..10)
 );
 
 Brand.create!(
   name: "Gymshark",
-  description: "Nike est un acteur majeur du marché du sportswear. La marque commercialise des vêtements, chaussures, équipements et accessoires de sport. Elle est présente dans plus de 80 pays et compte quelques grands noms dans le monde du football : l'attaquant brésilien Neymar Jr, le milieu de terrain français Paul Pogba, le défenseur allemand Mats Hummels, les attaquants argentins Lionel Messi et Luis Suarez ou encore l’ailier algérien Riyad Mahrez. Le club de football Manchester United est sous contrat avec Nike depuis 2006. En 2012, la société reçoit des critiques pour la suppression d'emplois dans ses usines en Chine.",
-  price: rand(1..99),
-  rank: rand(1..99),
-  quality: rand(1..99),
-  animal: rand(1..99),
-  labour: rand(1..99),
-  environment: rand(1..99)
+  description: "Gymshark est une marque de vêtements de fitness et d’accessoires qui a vu le jour en 2015. C’est une entreprise basée à Londres où elle propose ses produits sur toute la planète. C’est une entreprise qui commercialise des vêtements et des accessoires de fitness qui allient confort et style.",
+  price: rand(0..10),
+  rank: rand(0..10),
+  quality: rand(0..10),
+  animal: rand(0..10),
+  labour: rand(0..10),
+  environment: rand(0..10)
+);
+
+Brand.create!(
+  name: "Fabletics",
+  description: "Fabletics propose des tenues de sport de qualité, à des prix abordables. Depuis sa création, la marque a su toucher les femmes du monde entier avec ses produits et son code vestimentaire chic et décontracté.",
+  price: rand(0..10),
+  rank: rand(0..10),
+  quality: rand(0..10),
+  animal: rand(0..10),
+  labour: rand(0..10),
+  environment: rand(0..10)
+);
+
+Brand.create!(
+  name: "Asics",
+  description: "Asics est une marque de sport
+
+  qui a été créée en 1949 par Kihachiro Onitsuka. Le but de la marque était de créer des chaussures de sport qui prennent en compte la flexibilité et le confort. Le nom d’Asics vient du mot japonais « Aï » qui signifie lui-même « souplesse »",
+  price: rand(0..10),
+  rank: rand(0..10),
+  quality: rand(0..10),
+  animal: rand(0..10),
+  labour: rand(0..10),
+  environment: rand(0..10)
 );
