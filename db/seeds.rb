07e22a23ff4e264de607317267d370b88706b302
PierreLgx <97539842+Pierrooow@users.noreply.github.com>
2022-03-24 09:31:09 +0100

    add faker into seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,15 @@
 require 'faker'
 
-Brand.create!(
-    name: Faker::Company.name,
-    description: Faker::Company.suffix, 
-    price: rand(1..99),
-    rank: rand(1..99),
-    quality: rand(1..99),
-    animal: Faker::Company.sic_code,
-    labour: Faker::Food.dish,
-    environment: Faker::Cannabis.type
-)
+
+3.times do |t|
+    brand = Brand.create!(
+        name: Faker::Company.name,
+        description: Faker::Company.suffix, 
+        price: rand(1..99),
+        rank: rand(1..99),
+        quality: rand(1..99),
+        animal: Faker::Company.sic_code,
+        labour: Faker::Food.dish,
+        environment: Faker::Cannabis.type
+    );
+end
