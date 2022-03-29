9b5e18f7cfca331ee44d23a737ff0758b8bc537f
Benedicte Coulon <benedicte.coulon@wanadoo.fr>
2022-03-29 14:18:40 +0200

    brands added to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -86,6 +86,168 @@ Brand.create!(
   environment: rand(0..100)
 );
 
+Brand.create!(
+  name: "New Balance",
+  description: "Créée en 1906 à Boston, la marque fabriquait à cette époque des soutiens de voûte plantaire. Surtout spécialisée dans les chaussures de course à pied, aujourd'hui New Balance est la quatrième marque mondiale de chaussures de sport et elle est la seule à fabriquer des chaussures en Occident (70 % des chaussures de running vendues en Europe sont fabriquées en Angleterre.) ",
+  price: rand(0..100),
+  rank: rand(0..100),
+  quality: rand(0..100),
+  animal: rand(0..100),
+  labour: rand(0..100),
+  environment: rand(0..100)
+);
+
+Brand.create!(
+  name: "The North Face",
+  description: "The North Face, Inc. est une marque fondée par Douglas Tompkins, militant écologiste, spécialisée dans les vêtements et accessoires de sport, majoritairement orientée vers les activités dites de l'Outdoor, ainsi que le trail en complément dont il a sponsorisé pendant douze ans l'une des plus célèbres courses mondiales, l'Ultra-Trail du Mont-Blanc. (source: Wikipédia)",
+  price: rand(0..100),
+  rank: rand(0..100),
+  quality: rand(0..100),
+  animal: rand(0..100),
+  labour: rand(0..100),
+  environment: rand(0..100)
+);
+
+Brand.create!(
+  name: "Oakley",
+  description: "Oakley est une marque de lunettes créée par Jim Jannard en 1975 exploité par l'entreprise italienne Luxottica Group Milan. Le nom Oakley était le nom du chien de M. Jannard, qui se couchait souvent au pied d'un chêne, d'où le nom Oak-ley.
+  Historiquement, la marque permet d'équiper les cyclistes, lesquels, jusqu'au début des années 1980, n'avaient pas de lunettes de soleil spécifiques pour les protéger des rayons UV1.",
+  price: rand(0..100),
+  rank: rand(0..100),
+  quality: rand(0..100),
+  animal: rand(0..100),
+  labour: rand(0..100),
+  environment: rand(0..100)
+);
+
+Brand.create!(
+  name: "Buff",
+  description: "Un buff (marque déposée) est un vêtement multi-fonction, fabriqué et commercialisé depuis 1992 par la société espagnole Original Buff S.A. Composé d'un tissu élastique en microfibre de forme tubulaire, ce vêtement est utilisé comme protection contre les intempéries (froid, vent) sous forme de tour de cou, protège visage, cache-nez, cache-oreille, bandana, bonnet ou cagoule. Il peut aussi être utilisé comme bracelet de poignet ou pour soutenir les cheveux sous forme de bracelet serre-tête ou chouchou (source: Wikipédia)",
+  price: rand(0..100),
+  rank: rand(0..100),
+  quality: rand(0..100),
+  animal: rand(0..100),
+  labour: rand(0..100),
+  environment: rand(0..100)
+);
+
+Brand.create!(
+  name: "Arena",
+  description: "Arena est une entreprise fondée en 1973 par Horst Dassler et Adidas à Herzogenaurach en Allemagne. Dix-sept ans plus tard, Adidas la revend. De nos jours, l'entreprise Arena Italia S.p.A est à capital suisse et le siège se trouve en Italie, bien que depuis les années 2000, la marque ait changé plusieurs fois de propriétaires de différentes nationalités.
+  Arena est spécialisée dans les maillots de bain et accessoires de natation (du loisir à la compétition haut niveau). Elle est connue pour avoir équipé des nageurs comme Mark Spitz, Shane Gould, Matt Biondi, Oussama Mellouli, Alexander Popov, Laure Manaudou et récemment Alain Bernard3. Ses principaux concurrents sont Speedo, Tyr et Nabaiji (anciennement Tribord).(source: Wikipédia)",
+  price: rand(0..100),
+  rank: rand(0..100),
+  quality: rand(0..100),
+  animal: rand(0..100),
+  labour: rand(0..100),
+  environment: rand(0..100)
+);
+
+Brand.create!(
+  name: "Shimano",
+  description: "Shimano est une entreprise japonaise spécialisée dans la fabrication de pièces et composants pour cycles mais également pour la pêche, le snowboard et l'aviron. L’activité golf a été abandonnée en 2003 à la suite d'un échec commercial.
+  Shimano est leader mondial6 sur le segment des équipements pour vélos (dérailleurs, manettes de vitesses et de freins, pédales, roues, etc.). Ses activités cycles contribuent aux trois cinquièmes de son chiffre d’affaires. Shimano est également le leader dans le domaine des moulinets mais aussi des cannes à pêche (source: Wikipédia)",
+  price: rand(0..100),
+  rank: rand(0..100),
+  quality: rand(0..100),
+  animal: rand(0..100),
+  labour: rand(0..100),
+  environment: rand(0..100)
+);
+
+Brand.create!(
+  name: "Lacoste",
+  description: "Lacoste (anciennement La Chemise Lacoste) est une entreprise française, spécialisée dans la confection de prêt-à-porter masculin et féminin. L'entreprise est fondée en 1933 par André Gillier et René Lacoste, à la suite de la retraite du célèbre joueur de tennis. André Gillier est l'inventeur de la maille qui fit la renommée de la marque.
+  Lacoste est maintenant un groupe qui distribue vêtements, souliers, accessoires et parfums. La marque est rachetée en novembre 2012 par le groupe suisse Maus Frères Holding, qui détenait 35 % du capital, et qui acquiert le restant des parts pour un coût estimé à plus d'un milliard d'euros (source : Wikipédia)",
+  price: rand(0..100),
+  rank: rand(0..100),
+  quality: rand(0..100),
+  animal: rand(0..100),
+  labour: rand(0..100),
+  environment: rand(0..100)
+);
+
+Brand.create!(
+  name: "Le coq sportif",
+  description: "Le Coq Sportif est une marque de vêtements et de chaussures de sport. Fondée par Émile Camuset (en) en 1882 à Romilly-sur-Seine, dans l'Aube, la marque est l'une des premières marques de sport mondiale et devient rapidement la marque du sport français, accompagnant les amateurs, les professionnels, les clubs et les fédérations.
+  L'entreprise est particulièrement impliquée dans le sport professionnel mondial. Dans les années 1970 et 1980 la marque jouit d'une grande notoriété, en particulier auprès des consommateurs français. En tant qu'équipementier, elle habille la grande majorité des clubs de football et de rugby (source : wikipédia)",
+  price: rand(0..100),
+  rank: rand(0..100),
+  quality: rand(0..100),
+  animal: rand(0..100),
+  labour: rand(0..100),
+  environment: rand(0..100)
+);
+
+Brand.create!(
+  name: "Ripcurl",
+  description: "Rip Curl est une entreprise australienne de vêtements, d'accessoires et d'équipements de surf, fondée par deux surfeurs, Doug Warbrick et Brian Singer,3 de Torquay (Australie) (près de Geelong) qui commencèrent par fabriquer modestement leurs propres combinaisons de surf dans leur jardin. La compagnie est toujours basée à Torquay et sa filiale européenne est située en France, à Soorts-Hossegor.
+  Rip Curl est aujourd'hui l'une des principales marques dans le monde du surf et sponsorise un grand nombre de surfeurs professionnels et de compétitions comme le Rip Curl Pro. (source: Wikipédia)",
+  price: rand(0..100),
+  rank: rand(0..100),
+  quality: rand(0..100),
+  animal: rand(0..100),
+  labour: rand(0..100),
+  environment: rand(0..100)
+);
+
+Brand.create!(
+  name: "Fila",
+  description: "Fila est une marque italienne fondée à Biella dans le Piémont en 19111 par les frères Ettore et Giansevero Fila, spécialisée dans les vêtements de sport (source : Wikipédia)",
+  price: rand(0..100),
+  rank: rand(0..100),
+  quality: rand(0..100),
+  animal: rand(0..100),
+  labour: rand(0..100),
+  environment: rand(0..100)
+);
+
+Brand.create!(
+  name: "Wilson",
+  description: "Wilson Sporting Goods est un équipementier sportif américain, basé à Chicago.
+  Cette société est détenue depuis 1989 par le groupe finlandais Amer Sports, qui possède également les marques Atomic, Suunto, Precor, et la firme française Salomon.
+  Wilson fabrique des équipements pour de nombreux sports dont le badminton, le baseball, le softball, le basketball, le golf, le football américain, le squash, le tennis, et le volleyball. Wilson est un équipementier reconnu du football américain. (source : Wikipédia)",
+  price: rand(0..100),
+  rank: rand(0..100),
+  quality: rand(0..100),
+  animal: rand(0..100),
+  labour: rand(0..100),
+  environment: rand(0..100)
+);
+
+Brand.create!(
+  name: "Kappa",
+  description: "Kappa appartient au groupe Basic Net SpA, société italienne basée à Turin et propriétaire aussi des marques Robe di Kappa et Superga1. Aujourd’hui, l'entreprise est l'un des principaux équipementiers des clubs de Ligue 1 et de Ligue 2, de la Serie A du calcio itaien, Top 14, et est présente également, entre autres dans le cyclisme, le handball et le volley. La marque italienne est devenue le challenger des plus grandes marques comme Adidas, Nike ou Puma dans le domaine du sport en Europe (source : Wikipédia)",
+  price: rand(0..100),
+  rank: rand(0..100),
+  quality: rand(0..100),
+  animal: rand(0..100),
+  labour: rand(0..100),
+  environment: rand(0..100)
+);
+
+Brand.create!(
+  name: "Ellesse",
+  description: "Ellesse International est une entreprise italienne destinée à la mode du prêt-à-porter masculin et féminin. Elle est associée au tennis et au ski, deux sports appreciés de son fondateur et représentés dans le logo. (source : Wikipédia)",
+  price: rand(0..100),
+  rank: rand(0..100),
+  quality: rand(0..100),
+  animal: rand(0..100),
+  labour: rand(0..100),
+  environment: rand(0..100)
+);
+
+Brand.create!(
+  name: "Lafuma",
+  description: "Lafuma est un concepteur et fabricant français d'équipements de sport et de loisirs créé en 1930. Il est l'un des pionniers du matériel de sport de plein air. C'est une société holding cotée en bourse de Paris contrôlée par le groupe Suisse Calida. (source : Wikipédia)",
+  price: rand(0..100),
+  rank: rand(0..100),
+  quality: rand(0..100),
+  animal: rand(0..100),
+  labour: rand(0..100),
+  environment: rand(0..100)
+);
+
 Post.create!(
   title: "La marque Adidas est-elle souscieuse de l'environnement ?",
   description: "Adidas a éliminé certaines matières polluantes de sa production, notamment du  PVC dans toutes les catégories de produits qu’il fabrique. À présent, 99% des produits de la marque ne contiennent pas PVC. Adidas investit également dans le coton durable, en l’utilisant exclusivement et en encourageant les producteurs de ce type de coton en injectant des fonds dans le projet  << Initiative pour un Coton meilleur >>."
@@ -102,4 +264,5 @@ Post.create!(
   N°2: NOSC : Là encore, la marque propose des vêtements en Tencel®, Nylon et polyester recyclé. Plus original : NOSC a dernièrement développé la première tenue faite à partir … d’huile de ricin ! Hyper original, non ?
   N°3 : Circle : La marque donne accès à ses abonnées une tenue de sport échangeable et garantie à vie !"
 );
+
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
