ba29c252d731520873dfffbb1f561b8535f1125a
Benedicte Coulon <benedicte.coulon@wanadoo.fr>
2022-03-30 16:24:26 +0200

    deleted button deleted for unauthentificated used for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-  before_action :authenticate_user, only: [:new, :create, :show]
+  before_action :authenticate_user, only: [:new, :create, :show, :destroy]
 
   def create
     @post = Post.find(params[:post_id])
