42191406acd42e6628baded163edaf0c18525713
Benedicte Coulon <benedicte.coulon@wanadoo.fr>
2022-03-31 16:21:20 +0200

    mail rectified

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-    default from: 'fairathlete@gmail.com'
+    default from: 'niall.burdon@gmail.com'
 
     def welcome_email(user)
       #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
